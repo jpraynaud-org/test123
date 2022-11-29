67b3be2b6f48d163c7e73d9592cfd006076a9cda
Jean-Philippe Raynaud <jp.raynaud@gmail.com>
2022-11-29 14:40:40 +0100

    First commit

---
+++ demo/target/debug/deps/test123-0b586bf0955835e6.d
@@ -0,0 +1,7 @@
+/home/jp/Desktop/test123/demo/target/debug/deps/test123-0b586bf0955835e6.rmeta: src/main.rs
+
+/home/jp/Desktop/test123/demo/target/debug/deps/test123-0b586bf0955835e6.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
---
+++ demo/target/debug/deps/test123-cdc1641bd41552c2.d
@@ -0,0 +1,7 @@
+/home/jp/Desktop/test123/demo/target/debug/deps/test123-cdc1641bd41552c2.rmeta: src/main.rs
+
+/home/jp/Desktop/test123/demo/target/debug/deps/test123-cdc1641bd41552c2.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
